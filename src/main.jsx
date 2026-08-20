caa063160d9f12c62ca328d612f14003dc37f918
Mohamed <ghalleb.mohamed2@gmail.com>
2026-08-20 15:36:04 +0200

    Fix: ensure responsive navigation CSS is bundled on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./mobile-first.css";
+import "./mobile-nav-fix.css";
+import "./mobile-nav-layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
