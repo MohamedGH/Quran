ff5152996ac5ffc96efeb4c43c45d35321be22a4
Mohamed <ghalleb.mohamed2@gmail.com>
2026-08-21 20:27:01 +0200

    ci: add ESLint 9 flat configuration

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "android/**", "ios/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
+        console: "readonly",
+        URL: "readonly",
+        Blob: "readonly",
+        MediaRecorder: "readonly",
+        Audio: "readonly",
+        AudioContext: "readonly",
+        webkitAudioContext: "readonly",
+        localStorage: "readonly",
+        sessionStorage: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        requestAnimationFrame: "readonly",
+        cancelAnimationFrame: "readonly",
+        Capacitor: "readonly",
+        CSS: "readonly",
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "error",
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "error",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
